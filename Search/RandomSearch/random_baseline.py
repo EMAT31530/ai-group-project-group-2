6a0554eb84c0c6ddce0991d89870493d48e1f4a9
ih19708 <leonpl.799@gmail.com>
2022-02-22 17:14:08 +0000

    Fix error with plotting, comment typical values.

--- Search/RandomSearch/random_baseline.py
+++ Search/RandomSearch/random_baseline.py
@@ -6,14 +6,14 @@ import numpy as np
 from random_search import simulate
 
 guesses, win_percents, action_space = simulate("aback", epochs=1000)
-#fig, ax = plt.subplots(2)
+fig, ax = plt.subplots(2)
 
 # Guesses Figure
 guess_frequencies = collections.Counter(guesses)
 for i in guess_frequencies.keys():
     guess_frequencies[i] = guess_frequencies[i] / len(guesses)
 # ax[0].set_title("Guesses taken")
-"""ax[0].set_xticks(guess_frequencies.keys())
+ax[0].set_xticks(list(guess_frequencies.keys()))
 ax[0].set_ylabel("Frequency")
 ax[0].set_xlabel("Guesses")
 ax[0].bar(guess_frequencies.keys(), guess_frequencies.values())
@@ -23,9 +23,10 @@ ax[0].bar(guess_frequencies.keys(), guess_frequencies.values())
 ax[1].set_ylabel("Win Proportion")
 ax[1].set_xlabel("Games Played")
 ax[1].plot([i + 1 for i in range(len(win_percents))], win_percents)
-plt.tight_layout()"""
+plt.tight_layout()
 
-#plt.show()
+plt.show()
+plt.clf()
 
 n_games = len(win_percents)
 mean_actions = {i: action_space[i] / n_games for i in action_space.keys()}
@@ -35,4 +36,10 @@ plt.show()
 
 print(f"Guess frequencies: {guess_frequencies}")
 print(f"Mean Guesses: {np.mean(guesses)}")
-print(f"Win Proportion: {win_percents[-1] * 100}")
+print(f"Win Percent: {win_percents[-1] * 100} %")
+
+
+"""
+Mean Guesses: 5.2217573221757325
+Win Percent: 23.9 %
+"""
