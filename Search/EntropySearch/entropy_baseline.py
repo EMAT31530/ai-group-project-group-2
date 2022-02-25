62a44899905989d0c5a346af5a3686778a7ece38
ih19708 <leonpl.799@gmail.com>
2022-02-25 15:18:53 +0000

    Add entropy based search.

---
+++ Search/EntropySearch/entropy_baseline.py
@@ -0,0 +1,44 @@
+import collections
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from entropy_search import simulate
+
+guesses, win_percents, action_space = simulate("aback", epochs=1000)
+fig, ax = plt.subplots(2)
+
+# Guesses Figure
+guess_frequencies = collections.Counter(guesses)
+for i in guess_frequencies.keys():
+    guess_frequencies[i] = guess_frequencies[i] / len(guesses)
+# ax[0].set_title("Guesses taken")
+ax[0].set_xticks(list(guess_frequencies.keys()))
+ax[0].set_ylabel("Frequency")
+ax[0].set_xlabel("Guesses")
+ax[0].bar(guess_frequencies.keys(), guess_frequencies.values())
+
+# Win Percentages Figure
+#ax[1].set_title("Win %")
+ax[1].set_ylabel("Win Proportion")
+ax[1].set_xlabel("Games Played")
+ax[1].plot([i + 1 for i in range(len(win_percents))], win_percents)
+plt.tight_layout()
+
+plt.show()
+
+n_games = len(win_percents)
+mean_actions = {i: action_space[i] / n_games for i in action_space.keys()}
+
+plt.bar(mean_actions.keys(), mean_actions.values())
+plt.show()
+plt.clf()
+
+print(f"Guess frequencies: {guess_frequencies}")
+print(f"Mean Guesses: {np.mean(guesses)}")
+print(f"Win Percentage: {win_percents[-1] * 100}%")
+
+"""
+Mean Guesses: 4.684210526315789
+Win Percentage: 9.5%
+"""
---
+++ Search/EntropySearch/entropy_search.py
@@ -0,0 +1,52 @@
+import collections
+import pickle
+from typing import List
+
+from numpy.random import choice
+# import matplotlib.pyplot as plt
+import numpy as np
+
+from wordle import Wordle
+from Data.data import get_all_words
+from helpers import get_remaining_words_guess_known
+
+initial_word_list = get_all_words()
+random_answer = choice(initial_word_list)
+w = Wordle(random_answer)
+
+with open("entropy.pkl", "rb") as f:
+    entropy = pickle.load(f)
+
+
+def max_entropy_word(word_list: List[str]) -> str:
+    return max(word_list, key=lambda x: entropy[x])
+
+
+def simulate(start_word=None, epochs=100):
+    wins = 0
+    win_percents = []
+    guesses = []
+    action_space_size = {i: 0 for i in range(1, 7)}
+
+    for i in range(epochs):
+        word_list = initial_word_list.copy()
+        while not w.is_game_over:
+            if start_word and w.attempt_number == 0:
+                max_entropy_guess = start_word
+            else:
+                max_entropy_guess = max_entropy_word(word_list)
+            # print(random_guess)
+            _, guess_state, _ = w.guess_word(max_entropy_guess)
+            word_list = get_remaining_words_guess_known(word_list, guess_state)
+            action_space_size[w.attempt_number] += np.shape(word_list)[0]
+
+        if w.won:
+            # print(f"Attempt Number: {w.attempt_number}")
+            # print(f"Winning guess: {random_guess}")
+            # print("\n")
+            wins += 1
+            guesses.append(w.attempt_number)
+        win_percents.append(wins / (i + 1))
+        w.reset()
+
+    return guesses, win_percents, action_space_size
