c488a5ee4f297d7364935462ee57daa966ecd267
ih19708 <ih19708@bristol.ac.uk>
2022-03-04 14:59:25 +0000

    Plot search results.

---
+++ Search/plot_comparisons.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Search.save_results import get_results
+
+
+def plot_mean_guesses(data):
+    for n in data.keys():
+        plt.bar(n, sum(data[n][0]) / len(data[n][0]))
+    plt.title("")
+    plt.ylabel("Mean Guesses")
+    plt.xlabel("Search Method")
+    plt.show()
+    plt.clf()
+
+
+def plot_win_percentages(data):
+    for n in data.keys():
+        plt.plot([_ for _ in range(len(data[n][1]))], data[n][1], label=str(n))
+    plt.title("")
+    plt.ylabel("Win Rate")
+    plt.xlabel("Games Played")
+    plt.legend()
+    plt.show()
+    plt.clf()
+
+
+if __name__ == "__main__":
+    results = {
+        "Random": get_results("RandomSearch"),
+        "Letters": get_results("LetterFrequency"),
+        "Vowels": get_results("VowelSearch"),
+        #"Entropy": get_results("EntropySearch")
+    }
+
+    plt.style.use("seaborn-bright")
+    plot_win_percentages(results)
+    plot_mean_guesses(results)
