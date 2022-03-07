22b42a0585e1664e2f42058e72ea403320b84216
ih19708 <ih19708@bristol.ac.uk>
2022-03-07 12:36:38 +0000

    Add refactored word environment tuning and training.

---
+++ ReinforcementLearning/WordBasedRL/train_word_env.py
@@ -0,0 +1,34 @@
+import gym
+
+from ReinforcementLearning.training import train_model, get_best_params, get_smooth_curve, plot_learning_curve
+import ReinforcementLearning.WordBasedRL.wordle_gym
+from root import ROOT_DIR
+
+if __name__ == "__main__":
+    experiment_args = {"batch_size": 256,
+                       "n_steps": 64,
+                       "gamma": 0.995,
+                       "learning_rate": 0.0005537882778359183,
+                       "ent_coef": 0.003004556645041577,
+                       "clip_range": 0.4,
+                       "n_epochs": 5,
+                       "gae_lambda": 0.8,
+                       "max_grad_norm": 0.7,
+                       "vf_coef": 0.5253199800797684}
+
+    rewards = {50: 15.71}
+    n = 100
+    env = gym.make("WordleWord-v0")
+    env.change_num_words(n)
+    study_file_path = f"{ROOT_DIR}/ReinforcementLearning/WordBasedRL/studies/n={n}/study.pkl"
+    model_dir = f"{ROOT_DIR}/ReinforcementLearning/WordBasedRL/models/n={n}"
+    model_name = "best_model"
+    model_args = experiment_args if n == 300 else get_best_params(study_file_path)
+    #del model_args["net_arch"]
+    #del model_args["activation_fn"]
+    train_model(env=env, model_name=model_name, dir_path=model_dir, model_args=model_args,
+                time_steps=1000000)  # Saves model
+
+    x, y = get_smooth_curve(dir_path=model_dir)
+
+    plot_learning_curve(x, y)
---
+++ ReinforcementLearning/WordBasedRL/tune_word_env.py
@@ -0,0 +1,26 @@
+import os.path
+
+import matplotlib.pyplot as plt
+from optuna.visualization.matplotlib import plot_param_importances
+
+from ReinforcementLearning.hyperparameters import default_params
+from ReinforcementLearning.objective import Objective
+from ReinforcementLearning.tuning import study_hyperparameters, save_study
+from root import ROOT_DIR
+
+if __name__ == "__main__":
+    n = 100
+    objective = Objective(env_id="WordleWord-v0", ppo_hyperparameters_func=default_params, )
+
+    study_dir = f"{ROOT_DIR}/ReinforcementLearning/WordBasedRL/studies/n={n}/"
+    study_name = f"study.pkl"
+
+    while True:
+        study = study_hyperparameters(objective=objective, file_path=study_dir + study_name, n_trials=10, )
+        os.makedirs(study_dir, exist_ok=True)
+        save_study(study, study_file_path=study_dir + study_name,
+                   df_file_path=study_dir + f"{study_name}_results.csv")
+
+        plot_param_importances(study)
+        plt.title("n={n}")
+        plt.show()
