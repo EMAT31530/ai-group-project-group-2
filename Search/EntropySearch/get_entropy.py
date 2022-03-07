c49a27550e7e55bdb8f975b87b6f779a2de32819
ih19708 <ih19708@bristol.ac.uk>
2022-03-07 13:24:18 +0000

    Fix merging errors.

--- Search/EntropySearch/get_entropy.py
+++ Search/EntropySearch/get_entropy.py
@@ -2,6 +2,7 @@ import pickle
 from string import ascii_lowercase
 from scipy.stats import entropy
 
+
 def entropy_dic():
     # Find frequency of an alphabet
     AlphaList = ascii_lowercase
@@ -13,26 +14,24 @@ def entropy_dic():
     WordListSplit = ''.join(WordList)
     WordListSplit = list(WordListSplit)
 
-<<<<<<< HEAD:Search/get_entropy.py
     Total = len(WordListSplit)
     FreqList = []
     for a in AlphaList:
         Count = WordListSplit.count(a)
-        Freq = Count/Total
+        Freq = Count / Total
         FreqList.append(Freq)
-=======
-# Find frequency of an alphabet
-from root import ROOT_DIR
 
-AlphaList = ascii_lowercase
-with open('../../Data/wordle-answers-alphabetical.txt', 'r') as f:
-    WordList = f.readlines()
-with open('../../Data/wordle-allowed-guesses.txt', 'r') as f:
-    WordList += f.readlines()
-WordList = [i.strip('\n') for i in WordList]
-WordListSplit = ''.join(WordList)
-WordListSplit = list(WordListSplit)
->>>>>>> 6a0554eb84c0c6ddce0991d89870493d48e1f4a9:Search/EntropySearch/get_entropy.py
+    # Find frequency of an alphabet
+    from root import ROOT_DIR
+
+    AlphaList = ascii_lowercase
+    with open('../../Data/wordle-answers-alphabetical.txt', 'r') as f:
+        WordList = f.readlines()
+    with open('../../Data/wordle-allowed-guesses.txt', 'r') as f:
+        WordList += f.readlines()
+    WordList = [i.strip('\n') for i in WordList]
+    WordListSplit = ''.join(WordList)
+    WordListSplit = list(WordListSplit)
 
     # Calculate entropy of each word
     EntropyDic = {}
@@ -42,18 +41,14 @@ WordListSplit = list(WordListSplit)
             Frequency.append(FreqList[AlphaList.index(i)])
         EntropyDic[w] = entropy(Frequency, base=2)
 
-<<<<<<< HEAD:Search/get_entropy.py
-    # print(EntropyDic[max(EntropyDic)])
-=======
-# Calculate entropy of each word
-EntropyDic = {}
-for w in WordList:
-    Frequency = []
-    for i in list(w):
-        Frequency.append(FreqList[AlphaList.index(i)])
-    EntropyDic[w] = entropy(Frequency, base=2)
+        # print(EntropyDic[max(EntropyDic)])
+    # Calculate entropy of each word
+    EntropyDic = {}
+    for w in WordList:
+        Frequency = []
+        for i in list(w):
+            Frequency.append(FreqList[AlphaList.index(i)])
+        EntropyDic[w] = entropy(Frequency, base=2)
 
-print(EntropyDic[max(EntropyDic)])
-
-pickle.dump(EntropyDic, open('entropy.pkl', 'wb'))
->>>>>>> 6a0554eb84c0c6ddce0991d89870493d48e1f4a9:Search/EntropySearch/get_entropy.py
+    print(EntropyDic[max(EntropyDic)])
+    print({k: v for k, v in sorted(EntropyDic.items(), key=lambda item: item[1])})
