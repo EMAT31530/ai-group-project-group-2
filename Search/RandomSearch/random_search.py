3e43acc90f28bc0a820e3488806a909f4b0c3e2c
ih19708 <ih19708@bristol.ac.uk>
2022-03-07 12:42:58 +0000

    Fix how the mean search space size is calculated.

--- Search/RandomSearch/random_search.py
+++ Search/RandomSearch/random_search.py
@@ -15,8 +15,9 @@ def simulate(start_word=None, epochs=100):
     wins = 0
     win_percents = []
     guesses = []
-    action_space_size = {i: 0 for i in range(1, 7)}
-
+    action_space_size = {i: 0 for i in range(0, 7)}
+    action_space_size[0] = np.shape(initial_word_list)[0]
+    guess_freqs = {i: 0 for i in range(1, 7)}
     for i in range(epochs):
         word_list = initial_word_list.copy()
         while not w.is_game_over:
@@ -28,6 +29,7 @@ def simulate(start_word=None, epochs=100):
             _, guess_state, _ = w.guess_word(random_guess)
             word_list = get_remaining_words_guess_known(word_list, guess_state)
             action_space_size[w.attempt_number] += np.shape(word_list)[0]
+            guess_freqs[w.attempt_number] += 1
 
         if w.won:
             # print(f"Attempt Number: {w.attempt_number}")
@@ -37,5 +39,8 @@ def simulate(start_word=None, epochs=100):
             guesses.append(w.attempt_number)
         win_percents.append(wins / (i + 1))
         w.reset()
+
+    for k in range(1, 7):
+        action_space_size[k] = action_space_size[k] / guess_freqs[k]
 
     return guesses, win_percents, action_space_size
